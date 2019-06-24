73b64e2f8e7d2077dbeeb850c933f3794db89ef6
Adela Spirkova <ad.spirkova@gmail.com>
2019-06-24 10:52:23 +0200

    Student list with pics

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -7,6 +7,7 @@
 {{-- ----Q10------ --}}
 @foreach ($students as $student)
    <p>{{$student->name}}</p> 
+   <img class="portrait" src="{{ asset('img/'.$student->slug.'.jpg') }}" alt="" style="height:40px ; width:40px">
 @endforeach
 
 @endsection
